060478d8735590ea1164fb86052c4af99e7585a3
jacobeturpin <jacobeturpin@gmail.com>
2016-04-14 18:14:40 -0400

    Added sql script to create database structure

---
+++ sql/create_server.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Player (
+	Id			varchar(36)			NOT NULL,
+	Name		varchar(250)		NOT NULL,
+	Link		nvarchar(max)		NOT NULL
+);
+
+
+CREATE TABLE Team (
+	Id			varchar(36)			NOT NULL,
+	Name		varchar(250)		NOT NULL,
+	Link		nvarchar(max)		NOT NULL
+);
+
+/*
+CREATE TABLE Game (
+	Id			varchar(36)			
+);
+*/
