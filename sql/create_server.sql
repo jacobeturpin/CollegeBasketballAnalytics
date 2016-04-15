c13c3d48df1ddb9c0dde292b381988d3eabe0198
jacobeturpin <jacobeturpin@gmail.com>
2016-04-15 12:13:39 -0400

    Added game table to initial sql script

--- sql/create_server.sql
+++ sql/create_server.sql
@@ -11,8 +11,25 @@ CREATE TABLE Team (
 	Link		nvarchar(max)		NOT NULL
 );
 
-/*
+
 CREATE TABLE Game (
-	Id			varchar(36)			
+	Id				varchar(36)			NOT NULL,
+	"Date"			datetime			NOT NULL,
+	Link			nvarchar(max)		NOT NULL,
+	"Type"			integer				NOT NULL,
+	IsNeturalSite	bit					NOT NULL,
+	HomeTeamId		varchar(36)			NOT NULL,
+	HomeTeamScore	integer				NOT NULL,
+	AwayTeamId		varchar(36)			NOT NULL,
+	AwayTeamScore	integer				NOT NULL
 );
-*/
+
+/*
+CREATE TABLE PlayerBoxScore (
+	PlayerId	varchar(36)		NOT NULL,
+	GameId		varchar(36)		NOT NULL,
+	MP			integer			NOT NULL,
+	FGM			integer			NULL,
+	FGA			integer			NULL,
+	FGP			decimal			NULL,
+) */
