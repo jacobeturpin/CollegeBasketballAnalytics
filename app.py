e7dd123c29152b63001d4681ebab4f7e66cdbf7a
jacobeturpin <jacobeturpin@gmail.com>
2016-04-30 21:28:41 -0400

    Add beginning of flask for frontend web ui data management

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ from webscraper import WebScraperManager
 import statistics
 from config import *
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+
 if __name__ == '__main__':
-    database_manager = DatabaseManager()
-    web_scraper = WebScraperManager()
+    #database_manager = DatabaseManager()
+    #web_scraper = WebScraperManager()
+
+    app.run()
