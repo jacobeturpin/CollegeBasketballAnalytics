031951ddaa0dc7e12851ca461ac75a8ca332bf57
jacobeturpin <jacobeturpin@gmail.com>
2016-05-02 19:36:05 -0400

    Added to initial sql script that creates database structure

--- sql/create_server.sql
+++ sql/create_server.sql
@@ -1,22 +1,20 @@
 CREATE TABLE Player (
-	Id			varchar(36)			NOT NULL,
+	PlayerId	varchar(36)			NOT NULL,
 	Name		varchar(250)		NOT NULL,
 	Link		nvarchar(max)		NOT NULL
 );
-
 
 CREATE TABLE Team (
-	Id			varchar(36)			NOT NULL,
+	TeamId		varchar(36)			NOT NULL,
 	Name		varchar(250)		NOT NULL,
 	Link		nvarchar(max)		NOT NULL
 );
 
-
 CREATE TABLE Game (
-	Id				varchar(36)			NOT NULL,
-	"Date"			datetime			NOT NULL,
+	GameId			varchar(36)			NOT NULL,
+	GameDate		datetime			NOT NULL,
 	Link			nvarchar(max)		NOT NULL,
-	"Type"			integer				NOT NULL,
+	GameType		integer				NOT NULL,
 	IsNeturalSite	bit					NOT NULL,
 	HomeTeamId		varchar(36)			NOT NULL,
 	HomeTeamScore	integer				NOT NULL,
@@ -24,12 +22,68 @@ CREATE TABLE Game (
 	AwayTeamScore	integer				NOT NULL
 );
 
-
 CREATE TABLE PlayerBoxScore (
 	PlayerId	varchar(36)		NOT NULL,
+	TeamId		varchar(36)		NOT NULL,
 	GameId		varchar(36)		NOT NULL,
 	MP			integer			NOT NULL,
-	FGM			integer			NULL,
-	FGA			integer			NULL,
+	FGM			integer			NOT NULL,
+	FGA			integer			NOT NULL,
 	FGP			decimal			NULL,
+	TwoPM		integer			NOT NULL,
+	TwoPA		integer			NOT NULL,
+	TwoPP		decimal			NULL,
+	ThreePM		integer			NOT NULL,
+	ThreePA		integer			NOT NULL,
+	ThreePP		decimal			NULL,
+	FTM			integer			NOT NULL,
+	FTA			integer			NOT NULL,
+	FTP			decimal			NULL,
+	ORB			integer			NOT NULL,
+	DRB			integer			NOT NULL,
+	TRB			integer			NOT NULL,
+	AST			integer			NOT NULL,
+	STL			integer			NOT NULL,
+	BLK			integer			NOT NULL,
+	TOV			integer			NOT NULL,
+	PF			integer			NOT NULL,
+	PTS			integer			NOT NULL
+)
+
+CREATE TABLE TeamBoxScore (
+	TeamId		varchar(36)		NOT NULL,
+	GameId		varchar(36)		NOT NULL,
+	MP			integer			NOT NULL,
+	FGM			integer			NOT NULL,
+	FGA			integer			NOT NULL,
+	FGP			decimal			NULL,
+	TwoPM		integer			NOT NULL,
+	TwoPA		integer			NOT NULL,
+	TwoPP		decimal			NULL,
+	ThreePM		integer			NOT NULL,
+	ThreePA		integer			NOT NULL,
+	ThreePP		decimal			NULL,
+	FTM			integer			NOT NULL,
+	FTA			integer			NOT NULL,
+	FTP			decimal			NULL,
+	ORB			integer			NOT NULL,
+	DRB			integer			NOT NULL,
+	TRB			integer			NOT NULL,
+	AST			integer			NOT NULL,
+	STL			integer			NOT NULL,
+	BLK			integer			NOT NULL,
+	TOV			integer			NOT NULL,
+	PF			integer			NOT NULL,
+	PTS			integer			NOT NULL
+)
+
+CREATE TABLE Season(
+	Season			varchar(10)		NOT NULL,
+	StartDate		date			NOT NULL,
+	EndDate			date			NOT NULL,
+)
+
+CREATE TABLE GameType(
+	GameType		integer			NOT NULL,
+	Name			varchar(50)		NOT NULL
 )
