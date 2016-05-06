1672528a91ea0dfd7f50a541e15d84ef4deac134
jacobeturpin <jacobeturpin@gmail.com>
2016-05-05 22:50:46 -0400

    Added additional parameters to box score, team and player classes

--- boxscore.py
+++ boxscore.py
@@ -13,4 +13,5 @@ class PlayerBoxScore(BasicStatistics):
     def __init__(self, **kwargs):
         self.game_id = game_id
         self.player_id = player_id
+        self.team_id = team_id
         return super().__init__(**kwargs)
--- player.py
+++ player.py
@@ -3,5 +3,7 @@ class Player():
     def __init__(self, **kwargs):
 
         self.id = id
+        self.name = name
+        self.link = link
         self.games = []
         return super().__init__(**kwargs)
